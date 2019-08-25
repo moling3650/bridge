9c1e296288649876e63118e60b11a7da1b4b7fc6
moling <365024424@qq.com>
2019-08-25 18:24:16 +0800

    首页加入底部文字

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,8 @@
     <loader v-show="loadingVisiable" :is-loading="isLoading" @click-start="watchVideo"/>
     <page v-show="!loadingVisiable" ref="page" page-name="Home" can-skip @canplay="showVideo">
       <template slot-scope="{ isLoop }">
-        <router-link v-show="isLoop" class="start" to="/navigation">开始</router-link>
+        <router-link v-show="isLoop" class="start" to="/navigation">开始参观</router-link>
+        <p class="footer-text">指导单位：中共广东省委宣传部 中共广东省委网信办&nbsp;&nbsp;&nbsp;建设单位：南方新闻网</p>
       </template>
     </page>
   </div>
@@ -38,5 +39,14 @@ export default {
   font-size: 36px;
   color: #eee;
   text-decoration: none;
+}
+.footer-text {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  font-size: 24px;
+  text-align: center;
+  color: #eeeeee;
+  font-family: SimHei, Microsoft YaHei, serif;
 }
 </style>
