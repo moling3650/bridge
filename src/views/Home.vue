18a55b37075d5ee4e5b028830ca0ab6e3e861707
moling <365024424@qq.com>
2019-08-27 11:24:40 +0800

    更新首页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,12 @@
     <loader v-show="loadingVisiable" :is-loading="isLoading" @click-start="watchVideo"/>
     <page v-show="!loadingVisiable" ref="page" page-name="Home" can-skip @canplay="showVideo">
       <template slot-scope="{ isLoop }">
-        <router-link v-show="isLoop" class="start" to="/navigation">开始参观</router-link>
-        <p class="footer-text">指导单位：中共广东省委宣传部 中共广东省委网信办&nbsp;&nbsp;&nbsp;建设单位：南方新闻网</p>
+        <router-link v-show="isLoop" class="start" to="/navigation">
+          <img src="@/assets/img/start.png" alt="">
+        </router-link>
+        <p class="footer-text">
+          <img src="@/assets/img/footer-text.png">
+        </p>
       </template>
     </page>
   </div>
@@ -35,18 +39,20 @@ export default {
 <style lang="scss" scoped>
 .start {
   position: relative;
+  display: inline-block;
   top: 50%;
-  font-size: 36px;
-  color: #eee;
-  text-decoration: none;
+  width: 250px;
+  height: 100px;
+  img {
+    width: 100%;
+    height: 100%;
+  }
 }
 .footer-text {
   position: absolute;
   bottom: 0;
-  width: 100%;
-  font-size: 24px;
-  text-align: center;
-  color: #eeeeee;
-  font-family: SimHei, Microsoft YaHei, serif;
+  img {
+    width: 60%;
+  }
 }
 </style>
