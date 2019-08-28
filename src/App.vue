c141657f522725d7c57a2df3dab3e6e7af9cd3fb
moling <365024424@qq.com>
2019-08-28 15:16:02 +0800

    路由切换时加入渐入渐出的动画

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <audio autoplay loop :src="require('./assets/bgmusic.mp3')">
       你的浏览器版本太低，不支持audio标签
     </audio>
-    <router-view/>
+    <transition name="fade" mode="out-in">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
@@ -17,5 +19,11 @@ body,
   padding: 0;
   margin: 0;
   overflow: hidden;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
