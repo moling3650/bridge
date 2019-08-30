0cb940ff3b415aacb49050b5e4fea17036db0767
EvalGitHub <957010831@qq.com>
2019-08-30 23:24:25 +0800

    refactor: 添加jiaohu 页面

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app" :style="{ position: 'absolute', width: `${width}px`, height: `${height}px`, top: `${top}px`, left: `${left}px`, }">
-    <audio autoplay loop :src="require('./assets/bgmusic.mp3')">
+    <!-- <audio autoplay loop :src="require('./assets/bgmusic.mp3')">
       你的浏览器版本太低，不支持audio标签
-    </audio>
+    </audio> -->
     <transition name="fade" mode="out-in">
       <router-view v-if="showView"/>
     </transition>
