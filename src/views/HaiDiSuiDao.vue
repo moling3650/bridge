7adb9897a6c47a2dd9e2a5415ad53246349c2bdd
moling <365024424@qq.com>
2019-09-01 07:12:48 +0800

    海底隧道添加点击位

--- src/views/HaiDiSuiDao.vue
+++ src/views/HaiDiSuiDao.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="HaiDiSuiDao">
-    <page page-name="HaiDiSuiDao" autoplay can-skip>
-      <template slot-scope="{ isLoop }">
-        <nav-bar v-show="isLoop" back-and-next/>
+    <page page-name="HaiDiSuiDao" autoplay can-skip :opacity="opacity">
+      <template v-if="isLoop" slot-scope="{ isLoop }">
+        <dot :style="{ top: '5.7rem', left: '7rem' }" text="世界最长、最深的海底沉管隧道" @click.native="clickDot(1)"/>
+        <dot :style="{ top: '5.7rem', left: '10rem' }" text="世界最重沉管" @click.native="clickDot(2)"/>
+        <dot :style="{ top: '5.7rem', left: '13rem' }" text="世界首次实现沉管隧道滴水不漏" @click.native="clickDot(3)"/>
+        <dot :style="{ top: '2rem', left: '13rem' }" text="世界最精准的沉管对接" @click.native="clickDot(4)"/>
+        <dot :style="{ top: '3.5rem', left: '13rem' }" text="建设者访谈" @click.native="clickDot(5)"/>
+        <nav-barback-and-next/>
       </template>
     </page>
   </div>
@@ -11,5 +16,21 @@
 <script>
 export default {
   name: 'HaiDiSuiDao',
+  data () {
+    return {
+      opacity: 0,
+    }
+  },
+  methods: {
+    clickDot (index) {
+      // const video = {
+      //   url: require(`../../public/video/dot/4-${index}.mp4`),
+      // }
+      // this.opacity = 5
+      // this.$playVideo(video).then(() => {
+      //   this.opacity = 0
+      // })
+    },
+  },
 }
 </script>
