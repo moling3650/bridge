f78822d43bff713c663ea4b68901bab7f935aeb4
moling <365024424@qq.com>
2019-09-01 13:16:39 +0800

    添加Next组件

---
+++ src/components/Next.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="next" v-bind="$attrs"/>
+</template>
+
+<script>
+export default {
+  name: 'Next',
+}
+</script>
+
+<style lang="scss" scoped>
+.next {
+  position: absolute;
+  background-image: url(../assets/img/next.png);
+  background-size: contain;
+}
+</style>
