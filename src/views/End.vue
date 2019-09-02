f255b27d2dd78021de59c56501232c0f13f2e0b9
moling <365024424@qq.com>
2019-09-02 14:57:44 +0800

    修改结束页面的跳转逻辑

--- src/views/End.vue
+++ src/views/End.vue
@@ -1,11 +1,18 @@
 <template>
   <div id="End">
-    <page page-name="End" no-loop autoplay @ended="$router.push({ name: 'navigation', query: { loop: true } })"/>
+    <page page-name="End" no-loop autoplay @ended="isEnd = true ">
+      <back v-show="isEnd" :style="{ bottom: '0.25rem', right: '0.25rem', width: '1.2rem', height: '1.2rem' }" @click.native="$router.push({ name: 'navigation', query: { loop: true } })"/>
+    </page>
   </div>
 </template>
 
 <script>
 export default {
   name: 'End',
+  data () {
+    return {
+      isEnd: false,
+    }
+  },
 }
 </script>
