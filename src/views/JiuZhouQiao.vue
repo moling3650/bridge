ce6d7a625830000bbcc70563eedfc2c7150cea61
moling <365024424@qq.com>
2019-09-03 10:50:05 +0800

    文字修改

--- src/views/JiuZhouQiao.vue
+++ src/views/JiuZhouQiao.vue
@@ -5,7 +5,7 @@
         <nav-bar/>
         <dot :style="{ top: '1.1rem', left: '6.3rem' }" text="风帆塔" @click.native="clickDot(1)"/>
         <dot :style="{ top: '4.2rem', left: '10rem' }" text="世界最长的钢铁大桥" @click.native="showImages = true"/>
-        <dot :style="{ top: '3.8rem', left: '14.5rem' }" text="桥墩" @click.native="$router.push('/QiaoDun')"/>
+        <dot :style="{ top: '3.8rem', left: '14.5rem' }" text="桥墩建造" @click.native="$router.push('/QiaoDun')"/>
         <back :style="{ bottom: '0.2rem', right: '1.4rem', width: '1rem', height: '1rem' }" @click.native="$router.push({ name: 'navigation', query: { loop: true } })"/>
         <next :style="{ bottom: '0.2rem', right: '0.2rem', width: '1rem', height: '1rem' }" @click.native="$router.push('/JiangHaiQiao')"/>
       </template>
--- src/views/QingZhouQiao.vue
+++ src/views/QingZhouQiao.vue
@@ -5,7 +5,7 @@
         <nav-bar/>
         <dot :style="{ top: '2.2rem', left: '9.3rem' }" text="中国结" @click.native="clickDot(1)"/>
         <dot :style="{ top: '9rem', left: '3rem' }" text="SS级护栏" @click.native="clickDot(2)"/>
-        <dot :style="{ top: '9.3rem', left: '12rem' }" text="世界最大面积沥青铺装" @click.native="clickDot(3)"/>
+        <dot :style="{ top: '9.3rem', left: '12rem' }" text="世界最大单体钢桥面沥青铺装工程" @click.native="clickDot(3)"/>
         <back :style="{ bottom: '0.2rem', right: '1.4rem', width: '1rem', height: '1rem' }" @click.native="$router.push('/ShengTaiBaoHu')"/>
         <next :style="{ bottom: '0.2rem', right: '0.2rem', width: '1rem', height: '1rem' }" @click.native="$router.push('/XiRenGongDao')"/>
       </template>
