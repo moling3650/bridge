df0270a2f8524f0694935959329571b372b7ae37
moling <365024424@qq.com>
2019-09-04 22:58:21 +0800

    重构生态保护和青州桥

--- src/views/ShengTaiBaoHu.vue
+++ src/views/ShengTaiBaoHu.vue
@@ -3,10 +3,10 @@
     <page page-name="ShengTaiBaoHu" autoplay can-skip :opacity="opacity">
       <template v-if="isLoop" slot-scope="{ isLoop }">
         <nav-bar/>
-        <dot :style="{ top: '4rem', left: '12rem' }" text="保护海豚" @click.native="clickDot(1)"/>
+        <dot :style="{ top: '4rem', left: '12rem' }" text="保护海豚" @click.native="showVideo('3-1')"/>
         <dot :style="{ top: '3.6rem', left: '15.4rem' }" text="一图读懂生态保护"/>
-        <back :style="{ bottom: '0.2rem', right: '1.4rem', width: '1rem', height: '1rem' }" @click.native="$router.push('/JiangHaiQiao')"/>
-        <next :style="{ bottom: '0.2rem', right: '0.2rem', width: '1rem', height: '1rem' }" @click.native="$router.push('/QingZhouQiao')"/>
+        <back :style="{ bottom: '0.2rem', right: '1.4rem', width: '1rem', height: '1rem' }" @click.native="$redirect('/JiangHaiQiao')"/>
+        <next :style="{ bottom: '0.2rem', right: '0.2rem', width: '1rem', height: '1rem' }" @click.native="$redirect('/QingZhouQiao')"/>
       </template>
     </page>
   </div>
@@ -21,9 +21,9 @@ export default {
     }
   },
   methods: {
-    clickDot (index) {
+    showVideo (filename) {
       const video = {
-        url: require(`../../public/video/dot/3-${index}.mp4`),
+        url: require(`../../public/video/dot/${filename}.mp4`),
       }
       this.$audio.pause()
       this.opacity = 5
