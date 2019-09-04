a0f03d9c3108055a7b23f1147184e591882cbded
moling <365024424@qq.com>
2019-09-04 23:56:34 +0800

    重构人工岛和海底隧道页面

--- src/views/DongRenGongDao.vue
+++ src/views/DongRenGongDao.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="DongRenGongDao">
-    <page page-name="DongRenGongDao" autoplay can-skip :opacity="qrVisiable ? 5 : 0">
+    <page page-name="DongRenGongDao" autoplay can-skip :opacity="opacity">
       <template v-if="isLoop" slot-scope="{ isLoop }">
-        <dot :style="{ top: '2.1rem', left: '6.3rem' }" text="东人工岛展览馆" @click.native="showQRCode"/>
+        <dot :style="{ top: '2.1rem', left: '6.3rem' }" text="东人工岛展览馆" @click.native="show('qr')"/>
         <dot :style="{ top: '1.1rem', left: '13.3rem' }" text="港珠澳大桥4K超清壁纸"/>
-        <dot :style="{ top: '5.8rem', left: '9.5rem' }" text="港珠澳大桥建设大事记" @click.native="$router.push('/JianSheDaShiJi')"/>
+        <dot :style="{ top: '5.8rem', left: '9.5rem' }" text="港珠澳大桥建设大事记" @click.native="$redirect('/JianSheDaShiJi')"/>
         <nav-bar/>
-        <back :style="{ bottom: '0.2rem', right: '1.4rem', width: '1rem', height: '1rem' }" @click.native="$router.push('/HaiDiSuiDao')"/>
-        <next :style="{ bottom: '0.2rem', right: '0.2rem', width: '1rem', height: '1rem' }" @click.native="$router.push('/End')"/>
+        <back :style="{ bottom: '0.2rem', right: '1.4rem', width: '1rem', height: '1rem' }" @click.native="$redirect('/HaiDiSuiDao')"/>
+        <next :style="{ bottom: '0.2rem', right: '0.2rem', width: '1rem', height: '1rem' }" @click.native="$redirect('/End')"/>
       </template>
     </page>
-    <div v-if="qrVisiable" class="qr-mask" @click="qrVisiable = false">
+    <div v-if="qrVisiable" class="qr-mask" @click="hide('qr')">
       <div class="qr">
         <img src="@/assets/img/QRcode.png" alt="浏览展厅">
       </div>
@@ -23,12 +23,20 @@ export default {
   name: 'DongRenGongDao',
   data () {
     return {
+      opacity: 0,
       qrVisiable: false,
     }
   },
   methods: {
-    showQRCode () {
-      this.qrVisiable = true
+    show (key) {
+      this.opacity = 5
+      this.$audio.pause()
+      this[`${key}Visiable`] = true
+    },
+    hide (key) {
+      this.opacity = 0
+      this.$audio.play()
+      this[`${key}Visiable`] = false
     },
   },
 }
