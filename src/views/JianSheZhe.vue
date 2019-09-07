1f7d4a2eef8bdc19da211ef55e1b30f9f8b0559e
moling <365024424@qq.com>
2019-09-07 16:52:52 +0800

    完善建设者页面

--- src/views/JianSheZhe.vue
+++ src/views/JianSheZhe.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="JianSheZhe">
-    <back :style="{ top: '0.2rem', right: '1.4rem', width: '1rem', height: '1rem', zIndex: 1000 }" @click.native="$router.push(from)"/>
+    <back :style="{ bottom: '0.25rem', right: '0.25rem', width: '1.2rem', height: '1.2rem', zIndex: 1000 }" @click.native="$router.push(from)"/>
     <iframe width="100%" height="100%" src="http://news.southcn.com/n/node_395653.htm" frameborder="0"/>
   </div>
 </template>
@@ -23,5 +23,6 @@ export default {
 #JianSheZhe {
   width: 19.2rem;
   height: 10.8rem;
+  z-index: 999;
 }
 </style>
