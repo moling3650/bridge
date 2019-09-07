50a0454ed9f4a52932485ade92d1e75bfb2f9042
LiangGuanHui <1123770623@qq.com>
2019-09-08 00:38:17 +0800

    小地图

--- src/components/MiniMap.vue
+++ src/components/MiniMap.vue
@@ -13,14 +13,16 @@ export default {
   data () {
     return {
       links: [
+        { top: `1.52rem`, left: `0.22rem`, to: '/Home', text: '起始页' },
         { top: `0.87rem`, left: `0.58rem`, to: '/Roaming', text: '大桥漫游' },
-        { top: `1.5rem`, left: `1.12rem`, to: '/JiuZhouQiao', text: '九洲桥' },
-        { top: `0.37rem`, left: `1.62rem`, to: '/JiangHaiQiao', text: '江海桥' },
-        { top: `1.3rem`, left: `1.91rem`, to: '/ShengTaiBaoHu', text: '生态保护' },
-        { top: `0.94rem`, left: `2.43rem`, to: '/QingZhouQiao', text: '青州桥' },
-        { top: `0.05rem`, left: `2.87rem`, to: '/XiRenGongDao', text: '西人工岛' },
-        { top: `0.94rem`, left: `3.27rem`, to: '/HaiDiSuiDao', text: '海底隧道' },
+        { top: `1.44rem`, left: `1.05rem`, to: '/JiuZhouQiao', text: '九洲桥' },
+        { top: `0.37rem`, left: `1.53rem`, to: '/JiangHaiQiao', text: '江海桥' },
+        { top: `1.26rem`, left: `1.77rem`, to: '/ShengTaiBaoHu', text: '生态保护' },
+        { top: `0.91rem`, left: `2.3rem`, to: '/QingZhouQiao', text: '青州桥' },
+        { top: `0.06rem`, left: `2.69rem`, to: '/XiRenGongDao', text: '西人工岛' },
+        { top: `0.91rem`, left: `3.08rem`, to: '/HaiDiSuiDao', text: '海底隧道' },
         { top: `0.05rem`, left: `3.75rem`, to: '/DongRenGongDao', text: '东人工岛' },
+        { top: `0.69rem`, left: `4.2rem`, to: '/End', text: '结束页' },
       ],
     }
   },
