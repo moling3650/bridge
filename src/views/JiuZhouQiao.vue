171a2c2012ec93fa0f1619534349a091df09bf5a
moling <365024424@qq.com>
2019-09-10 20:53:38 +0800

    说明图片只出现一次

--- src/views/JiuZhouQiao.vue
+++ src/views/JiuZhouQiao.vue
@@ -2,7 +2,7 @@
   <div id="JiuZhouQiao">
     <page page-name="JiuZhouQiao" autoplay can-skip :opacity="opacity" @ended="handleEnd">
       <template v-if="isLoop" slot-scope="{ isLoop }">
-        <img v-show="showDesc" src="@/assets/img/desc.png" alt="desc" class="desc" @click="showDesc = false">
+        <img v-show="showDesc" src="@/assets/img/desc.png" alt="desc" class="desc" @click="closeDesc">
         <nav-bar/>
         <dot :style="{ top: '6.2rem', left: '3rem' }" text="世界最长跨海大桥" @click.native="$showFrame('http://newscenter.southcn.com/zt/20190902sjbri/index.html')"/>
         <dot :style="{ top: '1.1rem', left: '6.3rem' }" text="风帆塔" @click.native="showVideo('1-1')"/>
@@ -27,8 +27,12 @@ export default {
     }
   },
   methods: {
+    closeDesc () {
+      this.showDesc = false
+      sessionStorage.setItem('viewDesc', 'true')
+    },
     handleEnd () {
-      this.showDesc = true
+      this.showDesc = sessionStorage.getItem('viewDesc') !== 'true'
     },
     showVideo (filename) {
       this.$audio.pause()
