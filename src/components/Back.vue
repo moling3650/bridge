5f3e661ec7ed79548df97ee6296fe6dbb65243a6
moling <365024424@qq.com>
2019-09-13 21:44:47 +0800

    Back添加默认宽高

--- src/components/Back.vue
+++ src/components/Back.vue
@@ -11,6 +11,8 @@ export default {
 <style lang="scss" scoped>
 .back {
   position: absolute;
+  width: 1.2rem;
+  height: 1.2rem;
   background-image: url(../assets/img/back.png);
   background-size: contain;
   cursor: pointer;
