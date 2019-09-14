9e38d23fa79e73a8cccc7a71dbe7f141f9f52a89
moling <365024424@qq.com>
2019-09-14 09:58:56 +0800

    改造西人工岛页面适应导览版

--- src/views/XiRenGongDao.vue
+++ src/views/XiRenGongDao.vue
@@ -2,13 +2,18 @@
   <div id="XiRenGongDao">
     <page page-name="XiRenGongDao" autoplay can-skip :opacity="opacity">
       <template v-if="isLoop" slot-scope="{ isLoop }">
-        <nav-bar/>
-        <dot :style="{ top: '5rem', left: '7rem' }" text="人工岛透视" @click.native="show('subPage')"/>
-        <dot :style="{ top: '5rem', left: '15rem' }" text="人工岛施工图集" @click.native="showImages"/>
-        <dot :style="{ top: '7rem', left: '9rem' }" text="世界最大钢圆筒的海上之旅" @click.native="showVideo('5-3')"/>
-        <dot :style="{ top: '5rem', left: '11.4rem' }" text="世界首创钢圆筒快速筑岛技术" @click.native="showVideo('5-4')"/>
-        <dot :style="{ top: '6rem', left: '5rem' }" text="人工岛设计师专访" @click.native="showVideo('5-2')"/>
-        <mini-map/>
+        <template v-if="app.mode === 'zy'">
+          <dot :style="{ top: '5rem', left: '7rem' }" text="人工岛透视" @click.native="show('subPage')"/>
+          <dot :style="{ top: '5rem', left: '15rem' }" text="人工岛施工图集" @click.native="showImages"/>
+          <dot :style="{ top: '7rem', left: '9rem' }" text="世界最大钢圆筒的海上之旅" @click.native="showVideo('5-3')"/>
+          <dot :style="{ top: '5rem', left: '11.4rem' }" text="世界首创钢圆筒快速筑岛技术" @click.native="showVideo('5-4')"/>
+          <dot :style="{ top: '6rem', left: '5rem' }" text="人工岛设计师专访" @click.native="showVideo('5-2')"/>
+          <nav-bar/>
+          <mini-map/>
+        </template>
+        <template v-if="app.mode === 'dl'">
+          <guide-button v-show="guideBtnVisiable" class="rb" @click="nextStep">{{ guideTextList[step] }}</guide-button>
+        </template>
       </template>
     </page>
     <video
@@ -28,7 +33,8 @@
       <dot :style="{ top: '3.1rem', left: '8.1rem' }" text="环岛公路" @click.native="checkDetail(4)"/>
       <dot :style="{ top: '5rem', left: '6.6rem' }" text="防波堤" @click.native="checkDetail(3)"/>
       <dot :style="{ top: '7rem', left: '7.5rem' }" text="钢圆筒" @click.native="checkDetail(6)"/>
-      <back :style="{ bottom: '0.4rem', right: '0.4rem', width: '1rem', height: '1rem' }" @click.native="hide('subPage')"/>
+      <back v-if="app.mode === 'zy'" :style="{ bottom: '0.4rem', right: '0.4rem', width: '1rem', height: '1rem' }" @click.native="hide('subPage')"/>
+      <guide-button v-if="app.mode === 'dl'" class="rb" @click.native="hide('subPage')">继续</guide-button>
     </div>
   </div>
 </template>
@@ -38,24 +44,49 @@ import { images } from '@/assets/XiRenGongDao/data'
 
 export default {
   name: 'XiRenGongDao',
+  inject: ['app'],
   data () {
     return {
       subPageVisiable: false,
+      guideBtnVisiable: true,
       showDetail: false,
       opacity: 0,
       bgi: '',
+      guideTextList: [
+        '三维透视东、西人工岛构造',
+        '世界最大钢圆筒的海上之旅',
+        '215天如何快速围筑两个10万平米人工岛',
+        '“海上客厅”背后的1000+张设计图',
+        '海底沉管隧道',
+      ],
+      step: 0,
     }
   },
   methods: {
+    nextStep () {
+      const stepFun = [
+        () => this.show('subPage'),
+        () => this.showVideo('5-3'),
+        () => this.showVideo('5-4'),
+        () => this.showVideo('5-2'),
+        () => this.$router.push('/HaiDiSuiDao'),
+      ]
+      stepFun[this.step]()
+      this.step++
+    },
     showVideo (filename) {
+      this.guideBtnVisiable = false
       this.$audio.pause()
       const video = {
         url: require(`../../public/video/dot/${filename}.mp4`),
       }
       this.opacity = 5
       this.$video.play(video).then(() => {
+        this.guideBtnVisiable = true
         this.opacity = 0
-        this.$audio.play()
+        if (this.app.mode === 'zy') {
+          this.$audio.play()
+        }
       })
     },
     showImages () {
