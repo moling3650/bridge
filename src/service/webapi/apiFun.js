3598d3a1c180ccada35db5dca8bd0c8fbb0e7a65
LiangGuanHui <1123770623@qq.com>
2019-09-15 00:26:12 +0800

    样式

--- src/service/webapi/apiFun.js
+++ src/service/webapi/apiFun.js
@@ -1,52 +1,52 @@
-// import { CommentsApi } from './apiUrl'
-// import { projectId } from '../config'
-// import axios from './axiosSetting'
-// import jsonp from 'jsonp'
-// export const InteractiveApiFun = {
-//   releaseComment: function (data) {
-//     const url = CommentsApi.releaseComment + `&message=${data.message}&tid=${projectId.tid}&fid=${projectId.fid}`
-//     return new Promise((resolve, reject) => {
-//       jsonp(url, null, (err, res) => {
-//         if (err) {
-//           console.error(err.message)
-//           reject(err)
-//         } else {
-//           resolve(res)
-//         }
-//       })
-//     })
-//   },
-//   // 获取主题信息
-//   getThemeInfo: function (data) {
-//     const url = CommentsApi.getThemeInfo + `&tid=${projectId.tid}&fid=${projectId.fid}`
-//     return new Promise((resolve, reject) => {
-//       jsonp(url, null, (err, res) => {
-//         if (err) {
-//           console.error(err.message)
-//           reject(err)
-//         } else {
-//           resolve(res)
-//         }
-//       })
-//     })
-//   },
-//   // 点赞或者反对
-//   actionForRecommendOrDis: function (objData) {
-//     const url = CommentsApi.actionForRecommendOrDis + `&tid=${projectId.tid}&fid=${projectId.fid}&type=${objData.type}`
-//     return new Promise((resolve, reject) => {
-//       jsonp(url, null, (err, res) => {
-//         if (err) {
-//           console.error(err.message)
-//           reject(err)
-//         } else {
-//           resolve(res)
-//         }
-//       })
-//     })
-//   },
-//   // 获取已审核评论
-//   getApprovaledComments: function (objData) {
-//     const url = CommentsApi.getApprovalCommnets + `&tid=${projectId.tid}&fid=${projectId.fid}&ajax=1&offset=${objData.offset}&limit=${objData.limit}`
-//     return axios.get(url)
-//   },
-// }
+import { CommentsApi } from './apiUrl'
+import { projectId } from '../config'
+import axios from './axiosSetting'
+import jsonp from 'jsonp'
+export const InteractiveApiFun = {
+  releaseComment: function (data) {
+    const url = CommentsApi.releaseComment + `&message=${data.message}&tid=${projectId.tid}&fid=${projectId.fid}`
+    return new Promise((resolve, reject) => {
+      jsonp(url, null, (err, res) => {
+        if (err) {
+          console.error(err.message)
+          reject(err)
+        } else {
+          resolve(res)
+        }
+      })
+    })
+  },
+  // 获取主题信息
+  getThemeInfo: function (data) {
+    const url = CommentsApi.getThemeInfo + `&tid=${projectId.tid}&fid=${projectId.fid}`
+    return new Promise((resolve, reject) => {
+      jsonp(url, null, (err, res) => {
+        if (err) {
+          console.error(err.message)
+          reject(err)
+        } else {
+          resolve(res)
+        }
+      })
+    })
+  },
+  // 点赞或者反对
+  actionForRecommendOrDis: function (objData) {
+    const url = CommentsApi.actionForRecommendOrDis + `&tid=${projectId.tid}&fid=${projectId.fid}&type=${objData.type}`
+    return new Promise((resolve, reject) => {
+      jsonp(url, null, (err, res) => {
+        if (err) {
+          console.error(err.message)
+          reject(err)
+        } else {
+          resolve(res)
+        }
+      })
+    })
+  },
+  // 获取已审核评论
+  getApprovaledComments: function (objData) {
+    const url = CommentsApi.getApprovalCommnets + `&tid=${projectId.tid}&fid=${projectId.fid}&ajax=1&offset=${objData.offset}&limit=${objData.limit}`
+    return axios.get(url)
+  },
+}
